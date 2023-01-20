94a143fc9e7bd56c0a1f4e3484536b296662bf29
deejay-oukay <dj-ok@mail.ru>
2023-01-20 13:39:58 +0500

    Домашние задания выполнены до просмотра семинара. Возможно, будут изменения в процессе просмотра

---
+++ Task41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3 (В описании задачи ошибка: "-> 4", а не "-> 3")
+
+//число M заранее не определено
+int m = new Random().Next(5,10);
+int[] array = new int[m];
+
+int InputInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray()
+{
+    for (int i = 0; i < m; i++)
+        array[i] = InputInt($"Введите {(i+1)} число из {m}: ");
+}
+
+int OnlyPositive()
+{
+    int count = 0;
+    foreach (int item in array)
+        if (item > 0)
+            count++;
+    return count;
+}
+
+FillArray();
+
+Console.WriteLine("Положительных чисел: "+OnlyPositive());
+
---
+++ Task43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double InputDouble(string message)
+{
+    Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+string LineCrossing()
+{
+    double b1 = InputDouble("Введите значение b1: ");
+    double k1 = InputDouble("Введите значение k1: ");
+    double b2 = InputDouble("Введите значение b2: ");
+    double k2 = InputDouble("Введите значение k2: ");
+
+    if ((k1 == k2) && (b1 == b2))
+        return "Прямые - одинаковы";
+    else if (k1 == k2)
+        return "Прямые - параллельны";
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+        return "Прямые пересекаются в точке с координатами: X = " + x + ", Y = " + y;
+    }
+}
+
+Console.WriteLine(LineCrossing());
